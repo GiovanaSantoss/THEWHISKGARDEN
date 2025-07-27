0dacc277e4aae58777d0b6680101cdf95459da6c
Giovana Santoss <gigonzaga987@gmail.com>
2025-07-27 14:10:55 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Whisk Garden</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <div class="container">
+    <h1>The Whisk Garden</h1>
+    <p class="subtitle">Where recipes bloom naturally</p>
+
+    <div class="buttons">
+      <a href="login.php" class="btn">Login</a>
+      <a href="cadastro.php" class="btn">Sign Up</a>
+    </div>
+  </div>
+
+</body>
+</html>
